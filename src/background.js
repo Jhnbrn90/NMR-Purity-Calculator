8913229f081061d05767e9e214af40feb1f109ff
John Braun <john@brn.email>
2020-12-30 14:17:56 +0100

    Add auto-updating of the electronJS app

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { autoUpdater } from 'electron-updater'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
